547fdb31385e6c18071aac1b08291af186e7d233
alvaro <aaruizf@unal.edu.co>
2020-10-15 11:24:43 -0500

    Improved method to remove non digit characters from nodes

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,4 @@
-require '../lib/Scraper.rb'
+#require_relative '../lib/Scraper.rb'
 require 'cgi'
 require 'nokogiri'
 require 'open-uri'
@@ -30,18 +30,37 @@ base_url = 'https://www.ebay.com/sch/i.html?'
 
 puts "Welcome to my Ebay Web Scraper."
 
-doc = Nokogiri::HTML(URI.open("https://www.ebay.com/sch/i.html?&_nkw=computer"),Encoding::UTF_8.to_s)
+doc = Nokogiri::HTML(URI.open("https://www.ebay.com/sch/i.html?&_nkw=computer")) # Encoding::UTF_8.to_s
 to_select = "s-item__price"
 to_select2 = 'div[class="s-item__price"]'
 price = doc.xpath("//span[@class='s-item__price']")
-shipping = doc.xpath("//span[@class='s-item__shipping']")
+shipping = doc.xpath("//span[@class='s-item__shipping s-item__logisticsCost']")
+#shipping = doc.at("s-item__price")
+#puts shipping.is_a? Array
 #puts "The price is: #{price}"
-price1 = price[1].text.strip.sub!('Â','').gsub!("COP", "").gsub!(/[$,]/,'').gsub!(/\s+/, '').delete!("\u00A0")
+#price1 = price[1].text.strip.sub!('Â','').gsub!("COP", "").gsub!(/[$,]/,'').gsub!(/\s+/, '').delete!("\u00A0")
+class_name = 's-item__shipping '
+puts "Before..."
+#puts doc.xpath("//*[contains('s-item__shipping')]")
+#attempt1: shipping=doc.at('span:contains("s-item__shipping")')
+#attempt2 shipping = doc.xpath('//span[//*[contains(text(), "shipping")]]')
+#attempt3
+#puts doc.at_css("span")
+
+shipping1 = shipping[1].text.strip.sub!('Â','').gsub!("COP", "").gsub!(/[$,]/,'').gsub!(/\s+/, '').delete!("\u00A0")
+puts "shipping1, pre gsub: #{shipping1}"
+#attempt1 shipping1 = shipping[1].text.gsub("[^0-9.-]",'')
+#attempt2
+shipping1 = shipping[1].text.delete('^0-9.')
+price1 = price[1].text.delete('^0-9.')
+puts "shipping1, post gsub #{shipping1}"
 puts "price[1] is: #{price1}"
-puts "shipping[1] is: #{shipping} \s\n"
+puts "shipping[1] is: #{shipping1} \s\n"
 #puts price[1]
 puts "\u00A0"
 encoded_text = 'COP $538&Acirc;&nbsp;100.81'
 decoded_text = CGI::unescape(encoded_text)
 puts decoded_text
+puts "The doc is"
+#puts doc
 #puts doc
--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -1,5 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
+require 'histogram/array'
+require 'ascii_charts'
 class Scraper
   def initialize(url='')
     #@doc = Nokogiri::HTML(URI.open(url),Encoding::UTF_8.to_s)
@@ -26,15 +28,12 @@ class Scraper
     return arr
   end
 
-
-  def mean(prices)
-
+  def mean(arr)
+    return arr.sum / arr.length
   end
 
   def median(prices)
     prices = prices.sort
-    #prices = convert_to_f(prices)
-    puts "Prices are: #{prices}"
     if prices.length.odd?
       return prices[prices.length/2].to_f
     else
@@ -42,12 +41,58 @@ class Scraper
     end
   end
 
-  def std_dev(prices)
+  def sample_stdev(prices)
+    x = mean(prices)
+    sum_squares = 0
+    prices.each {|el| sum_squares += (el - x)**2}
+    sample_variance = sum_squares / (prices.length - 1)
+    return Math.sqrt(sample_variance)
   end
 
   def max_min(prices)
+    return prices.min, prices.max
+  end
+
+=begin
+  def calculate_bins(prices)
+    mid = median(prices)
+    n = prices.length
+    if prices.length.even?
+      subarray1 = prices[0..((n/2)-1)]
+      subarray2 = prices[((n/2))..-1]
+    else
+      subarray1 = prices[0..((n/2))]
+      subarray2 = prices[((prices.length/2))..-1]
+    end
+    q1 = median(subarray1)
+    q3 = median(subarray2)
+    iqr = q3 - q1
+    h = 2*iqr*(n**(1/3))
+    puts "h is: #{h}"
+    puts "prices.max is : #{prices.max}"
+    puts "prices.min is : #{prices.min}"
+    return ((prices.max - prices.min) / h).round
+  end
+=end
+
+  def prepare_histogram_array(bins, freqs)
+    bins.each{|el| el.round(2)}
+    freqs.each{|el| el.round(2)}
+    return bins.zip(freqs)
   end
 end
-prices = [1, 3, 4, 6, 7, 8, 9, 20]
+prices = Array.new(200) { rand(1...200) }
+puts "Prices are: #{prices}"
  new_scraper = Scraper.new
- puts new_scraper.median(prices)
+ puts "Median: #{new_scraper.median(prices)}"
+ puts "Mean: #{new_scraper.mean(prices)}"
+ puts "Stdev: #{new_scraper.sample_stdev(prices)}"
+ puts "The histogram is: "
+ #puts new_scraper.calculate_bins(prices)
+ #num_bins = new_scraper.calculate_bins(prices)
+ (bins, freqs) = prices.histogram
+ my_arr = new_scraper.prepare_histogram_array(bins, freqs)
+ puts "The bins and freqs are: "
+ puts bins, freqs
+ my_chart = AsciiCharts::Cartesian.new(my_arr)
+ my_chart.draw
